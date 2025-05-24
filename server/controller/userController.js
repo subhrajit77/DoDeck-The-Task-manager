58036e447382f200db4b5eb2b1b895082f546a3a
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2025-05-24 11:43:41 +0530

    add: controller fucntion

---
+++ server/controller/userController.js
@@ -0,0 +1,186 @@
+import User from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_here";
+const TOKEN_EXPIRES = "24h";
+
+const createToken = (userId) =>
+    jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: TOKEN_EXPIRES });
+
+//register function
+
+export async function registerUser(req, res) {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please fill all fields" });
+    }
+    if (!validator.isEmail(email)) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please enter a valid email" });
+    }
+    if (!password.length < 8) {
+        return res
+            .status(400)
+            .json({
+                success: false,
+                message: "Password must be max 8 characters",
+            });
+    }
+
+    try {
+        if (await User.findOne({ email })) {
+            return res
+                .status(409)
+                .json({ success: false, message: "User already exists" });
+        }
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashed,
+        });
+        const token = createToken(user._id);
+        res.status(201).json({
+            succcess: true,
+            token,
+            user: { id: user.name, name: user.name, email: user.email },
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+
+//Login Function
+
+export async function loginUser(req, res) {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please fill all fields" });
+    }
+    if (!validator.isEmail(email)) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please enter a valid email" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(401)
+                .json({ success: false, message: "Invalid credentials" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res
+                .status(401)
+                .json({ success: false, message: "Invalid password" });
+        }
+        const token = createToken(user._id);
+        res.status(200).json({
+            success: true,
+            token,
+            user: { id: user._id, name: user.name, email: user.email },
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+
+//Get User Function
+export async function getUser(req, res) {
+    try {
+        const user = await user.findById(req.user.id).select("name email");
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: "User not found" });
+        }
+        res.json({ success: true, user });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+
+//update user profile
+
+export async function updateProfile(req, res) {
+    const { name, email } = req.body;
+
+    if (!name || !email || !validator.isEmail(email)) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please fill all fields" });
+    }
+
+    try {
+        const exists = await User.findOne({ email, _id: { $ne: req.user.id } });
+        if (exists) {
+            return res
+                .status(409)
+                .json({
+                    success: false,
+                    message: "Email already used by another account",
+                });
+        }
+        const user = await User.findByIdAndUpdate(
+            req.user.id,
+            { name, email },
+            { new: true, runValidators: true, select: "name email" }
+        );
+        res.json({ success: true, user });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
+
+//change password function
+export async function updatePassword(req, res) {
+    const { currentPassword, newPassword } = req.body;
+
+    if (!currentPassword || !newPassword || newPassword) {
+        return res
+            .status(400)
+            .json({ success: false, message: "Please fill all fields" });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select("password");
+
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: "User not found" });
+        }
+
+        const match = await bcrypt.compare(currentPassword, user.password);
+        if (!match) {
+            return res
+                .status(401)
+                .json({
+                    success: false,
+                    message: "Current password incorrect",
+                });
+        }
+        user.password = await bcrypt.hash(newPassword, 10);
+        await user.save();
+        res.json({ success: true, message: "Password updated successfully" });
+    } 
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+}
